d832f2c6bdce839efc3d97dd48c893a18c6702b8
Gayaneh Taturyan <43474745+taturyan@users.noreply.github.com>
2021-07-09 05:13:14 +0400

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fairness',
+      version='0.1',
+      description='Package on fairness.',
+      url='http://github.com/taturyan/fairness',
+      author='Gayane Taturyan',
+      author_email='gayaneh.taturyan@gmail.com',
+      license='MIT',
+      packages=['fairness'],
+      zip_safe=False)
