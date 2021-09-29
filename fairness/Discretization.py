e773bb50f884c1ea24f766e11f0484da6248ed90
gayaneh <gayaneh.taturyan@gmail.com>
2021-09-29 15:14:18 +0200

    added initial "without sens" algo

--- fairness/Discretization.py
+++ fairness/Discretization.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.special import softmax
+import collections
 
 
 class FairRegressionDiscret():
@@ -80,3 +81,90 @@ class FairRegressionDiscret():
         z[X[:,self.sens_index] == 1, :] *= self.weights[1]
 
         return (np.argmin(-s[:,np.newaxis] * self.coef_ + z, axis=1) - self.L) * self.M / self.L
+
+
+
+
+class FairRegressionDiscret_without_sens():
+
+    def __init__(self, base_method, classifier, beta=1, L=20, num_iter=1000, M=10, weights=[.5, .5]):
+        """
+        Parameters
+        ----------
+        base_method : TYPE
+            Description
+        beta : int, optional
+            Description
+        L : int, optional
+            Description
+        num_iter : int, optional
+            Description
+        M : int, optional
+            Description
+        weights : list, optional
+            Description
+        """
+
+        self.base_method = base_method
+        self.classifier = classifier
+        self.beta = beta
+        self.L = L
+        self.num_iter = num_iter
+        self.M = M
+        self.weights = weights
+
+    def fit(self, X_unlab):
+        """Summary
+
+        Parameters
+        ----------
+        X_unlab : TYPE
+            Description
+        """
+        coef = np.zeros(2 * self.L + 1)
+        moment = np.zeros(2 * self.L + 1)
+
+        reg_pred = self.base_method.predict(X_unlab)
+        clf_prob = self.classifier.predict_proba(X_unlab)
+        tau_X = clf_prob[:, list(self.classifier.classes_).index(1)].reshape([-1,1])
+        #p1 = collections.Counter(S)[ind]/len(S)
+        p1 = self.weights[1]
+
+        discr = np.arange(-self.L, self.L + 1) * self.M / self.L
+        z = np.square(reg_pred[:, np.newaxis] - discr)
+
+        tau = 0
+        for t in range(self.num_iter):
+            tmp = (1 + np.sqrt(1 + 4 * tau ** 2)) / 2
+            gamma = (1 - tau) / tmp
+            tau = tmp
+            coef_prev = coef
+            coef = moment - (self.beta / 2) * \
+                   np.mean(softmax(((1-tau_X/p1)*moment - z) /
+
+                        self.beta, axis=1), axis=0)
+            moment = (1 - gamma) * coef + gamma * coef_prev
+        self.coef_ = coef
+        self.discr_ = discr
+        self.p1_ = p1
+
+    def predict(self, X):
+        """Summary
+
+        Parameters
+        ----------
+        X : TYPE
+            Description
+
+        Returns
+        -------
+        TYPE
+            Description
+        """
+        n_samples, _ = X.shape
+
+        z = np.square(self.base_method.predict(X)[:, np.newaxis] - self.discr_)
+        clf_prob = self.classifier.predict_proba(X)
+        tau_X = clf_prob[:, list(self.classifier.classes_).index(1)].reshape([-1,1])
+
+        return (np.argmin(z + self.coef_ * (tau_X/self.p1_ - 1), axis=1) - self.L) * self.M / self.L
